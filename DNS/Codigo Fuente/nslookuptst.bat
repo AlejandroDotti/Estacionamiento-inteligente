8ec8fe90d3255a6ee03c1fa1a05eb0ba5f07b338
AlejandroDotti <72307323+AlejandroDotti@users.noreply.github.com>
2020-10-03 21:14:22 -0300

    Add files via upload

---
+++ DNS/Codigo Fuente/nslookuptst.bat
@@ -0,0 +1,13 @@
+
+nslookup dns.unm.ar
+nslookup barrera1.unm.ar			
+nslookup barrera2.unm.ar			
+nslookup barrera3.unm.ar			
+nslookup barrera4.unm.ar			
+nslookup servidorobjdistribuidos.unm.ar
+nslookup manipulador.unm.ar			
+nslookup sensor1.unm.ar				
+nslookup sensor2.unm.ar				
+nslookup sensor3.unm.ar				
+nslookup sensor4.unm.ar				
+nslookup videomulticast.unm.ar
